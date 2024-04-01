d7f957d7b08968186bd91c630aa1660e6802364b
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-04-01 13:19:11 -0600

    Add ticker route

--- server.js
+++ server.js
@@ -5,4 +5,13 @@ app.get("/", (req, res) => {
   res.status(200).send("<h1>Stock Data API</h1>");
 });
 
+app.get("/:ticker", (req, res) => {
+  const { ticker } = req.params;
+  const { key } = req.query;
+
+  if (!ticker || !key) {
+    res.status(400).send({ messsage: "Please provide key and ticker" });
+  }
+});
+
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
