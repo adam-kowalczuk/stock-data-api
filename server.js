75dd9e2c7710a5263088b109439e690716dad080
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2024-04-01 13:16:07 -0600

    Add home route

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const app = require("express")();
 const port = process.env.PORT || 8008;
 
+app.get("/", (req, res) => {
+  res.status(200).send("<h1>Stock Data API</h1>");
+});
+
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
